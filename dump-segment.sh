bc5f9f6fc517fe43c9475fc295472acf62f64bad
Hellmar Becker <becker@hellmar-becker.de>
2021-11-28 18:13:45 +0100

    Update dump-segment.sh

--- dump-segment.sh
+++ dump-segment.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-java -classpath "/Users/hellmarbecker/apache-druid-0.22.0/lib/*" -Ddruid.extensions.loadList="[]" org.apache.druid.cli.Main \
+
+DRUID_HOME="/Users/hellmarbecker/apache-druid-0.22.0"
+
+java -classpath "${DRUID_HOME}/lib/*" -Ddruid.extensions.loadList="[]" org.apache.druid.cli.Main \
 tools dump-segment \
   --directory /Users/hellmarbecker/tmpwork \
   --out ~/tmpwork/output.txt
