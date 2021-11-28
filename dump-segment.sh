454d52bf853390cd47619d80828ab5d27248e1ad
Hellmar Becker <becker@hellmar-becker.de>
2021-11-28 19:36:56 +0100

    Update dump-segment.sh

--- dump-segment.sh
+++ dump-segment.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-DUMP_TMPDIR=~/tmpwork
+# path settings for default quickstart install
+
 DRUID_HOME="/Users/hellmarbecker/apache-druid-0.22.0"
+DRUID_DATADIR="${DRUID_HOME}/var/druid/segments"
+DRUID_DATASOURCE="wikipedia"
+DUMP_TMPDIR=~/tmpwork
 
 java -classpath "${DRUID_HOME}/lib/*" -Ddruid.extensions.loadList="[]" org.apache.druid.cli.Main \
 tools dump-segment \
