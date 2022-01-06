16a4e856f1f26adab4ce77df6a0afa7f033bcee7
hellmarbecker <becker@hellmar-becker.de>
2022-01-06 12:04:51 +0100

    Add Top N Channel report

--- dump-segment.sh
+++ dump-segment.sh
@@ -7,17 +7,28 @@ DRUID_DATADIR="${DRUID_HOME}/var/druid/segments"
 DRUID_DATASOURCE="wikipedia-dynamic-1"
 DUMP_TMPDIR="${HOME}/tmpwork"
 
+TOPN=5
+
 FILES=$(cd ${DRUID_DATADIR} && find ${DRUID_DATASOURCE} -type f)
 
 for f in $FILES; do
-  FDIR=$(dirname $f)
+  FDIR=$(dirname $f)  
   FBASE=$(basename $f)
+  PARTN=$(cut -d "/" -f 4 <<<$f)
   ODIR=${DUMP_TMPDIR}/$FDIR
   mkdir -p $ODIR
-  unzip ${DRUID_DATADIR}/$f -d $ODIR
+  unzip -q -o -f ${DRUID_DATADIR}/$f -d $ODIR
 
   java -classpath "${DRUID_HOME}/lib/*" -Ddruid.extensions.loadList="[]" org.apache.druid.cli.Main \
     tools dump-segment \
     --directory $ODIR \
     --out $ODIR/output.txt
+
+  # Top Countries Report
+  echo Partition $PARTN:
+  jq -r ".channel" $ODIR/output.txt | sort | uniq -c | sort -nr | head -$TOPN
+
 done
+
+
+
