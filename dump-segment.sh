928b583cc310149222b86c720556e2ebcf5ee31c
hellmarbecker <becker@hellmar-becker.de>
2022-01-06 11:35:27 +0100

    Add loop to unzip and process all files in a datasource

--- dump-segment.sh
+++ dump-segment.sh
@@ -2,12 +2,22 @@
 
 # path settings for default quickstart install
 
-DRUID_HOME="/Users/hellmarbecker/apache-druid-0.22.0"
+DRUID_HOME="${HOME}/apache-druid-0.22.1"
 DRUID_DATADIR="${DRUID_HOME}/var/druid/segments"
-DRUID_DATASOURCE="wikipedia"
-DUMP_TMPDIR=~/tmpwork
+DRUID_DATASOURCE="wikipedia-dynamic-1"
+DUMP_TMPDIR="${HOME}/tmpwork"
 
-java -classpath "${DRUID_HOME}/lib/*" -Ddruid.extensions.loadList="[]" org.apache.druid.cli.Main \
-tools dump-segment \
-  --directory /Users/hellmarbecker/tmpwork \
-  --out ~/tmpwork/output.txt
+FILES=$(cd ${DRUID_DATADIR} && find ${DRUID_DATASOURCE} -type f)
+
+for f in $FILES; do
+  FDIR=$(dirname $f)
+  FBASE=$(basename $f)
+  ODIR=${DUMP_TMPDIR}/$FDIR
+  mkdir -p $ODIR
+  unzip $f -d $ODIR
+
+  java -classpath "${DRUID_HOME}/lib/*" -Ddruid.extensions.loadList="[]" org.apache.druid.cli.Main \
+    tools dump-segment \
+    --directory $ODIR \
+    --out $ODIR/output.txt
+done
