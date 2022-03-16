a71c950ea6ba3fd209193d2bd4f3913b1f5861e6
hellmarbecker <becker@hellmar-becker.de>
2022-03-16 09:10:49 +0100

    Allow different distributions for each group

--- randstream.py
+++ randstream.py
@@ -4,11 +4,19 @@ import json
 
 def main():
  
+    weights = {
+        'A': (10000, 1000),
+        'B': (10000, 2000),
+        'C': (10000, 5000)
+        'D': (20000, 2000),
+        'E': (25000, 2500),
+    }
     while True:
+        grp = random.choices('ABCDE', cum_weights=(0.50, 0.75, 0.82, 0.91, 1.00), k=1)[0]
         rec = {
             'tn': time.time(),
-            'cn': random.choices('ABCDE', cum_weights=(0.50, 0.75, 0.82, 0.91, 1.00), k=1)[0],
-            'rn': random.gauss(0.0, 1.0)
+            'cn': grp,
+            'rn': random.gauss(*weights[grp])
         }
         print(json.dumps(rec))
         time.sleep(0.1)
