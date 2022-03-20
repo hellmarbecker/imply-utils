e4e4991d481eeb90b907232339583563ed6c5c34
hellmarbecker <becker@hellmar-becker.de>
2022-03-20 12:07:17 +0100

    Add various distributions

--- randstream.py
+++ randstream.py
@@ -2,24 +2,32 @@ import time
 import random
 import json
 
+
+def bimodal(m1, m2, s, p):
+
+    if random.random() <= p:
+        m = m1
+    else:
+        m = m2
+    return random.gauss(m, s)
+
+
 def main():
  
-    weights = {
-        'A': (10000, 1000),
-        'B': (10000, 2000),
-        'C': (10000, 5000)
-        'D': (20000, 2000),
-        'E': (25000, 2500),
+    distr = {
+        'A': { 'fn': random.gauss,       'param': (10000, 2000) },
+        'B': { 'fn': random.gauss,       'param': (10000, 10000) },
+        'C': { 'fn': random.expovariate, 'param': (0.0001,) },
+        'D': { 'fn': bimodal,            'param': (10000, 50000, 2000, 0.8) },
     }
-    while True:
-        grp = random.choices('ABCDE', cum_weights=(0.50, 0.75, 0.82, 0.91, 1.00), k=1)[0]
+    for i in range(0, 10000):
+        grp = random.choices('ABCD', cum_weights=(0.50, 0.75, 0.90, 1.00), k=1)[0]
         rec = {
-            'tn': time.time(),
+            'tn': time.time() - 10000 + i,
             'cn': grp,
-            'rn': random.gauss(*weights[grp])
+            'rn': distr[grp]['fn'](*distr[grp]['param'])
         }
         print(json.dumps(rec))
-        time.sleep(0.1)
 
 if __name__ == "__main__":
     main()
