f910db24c09e03cc79ac6d82d472a02d8b875b39
hellmarbecker <becker@hellmar-becker.de>
2022-09-11 20:08:26 +0200

    Cleanup

--- gamble-cohort.pl
+++ gamble-cohort.pl
@@ -7,7 +7,6 @@ use warnings;
 
 use Getopt::Std;
 use DateTime;
-use Data::Dumper;
 
 my $start = DateTime->new(
     day   => 1,
@@ -40,7 +39,6 @@ for my $i (0 .. $numPlayers-1 ) {
     my $playerAgeNow = int(rand($playerMaxAge));
     my $signupDate = $today - DateTime::Duration->new(days => $playerAgeNow);
     $playerSignup[$i] = $signupDate;
-    # say "$playerID $signupDate $playerAgeNow";
 }
 
 say "curdate,playerid,signupdate,deposit";
@@ -51,12 +49,12 @@ for ( my $d = $start->clone; $d < $stop; $d->add(days => 1) ) {
     while ($n < $numEventsMax) {
         my $ind = int(rand($numPlayers));
         my $playerID = $players[$ind];
-        # my $playerAge = ($d - $playerSignup[$ind])->days;
         next if ( $playerSignup[$ind] > $d ); # player did not exist yet
+
         my $playerAge = $d->delta_days($playerSignup[$ind])->in_units('days');
         my $cooldown = exp(-3.0 * $playerAge / $playerMaxAge);
         next if ( rand(1.0) > $cooldown ); # simulate that players churn off slowly
-        # say "$d $n $ind $playerID $playerSignup[$ind] $playerAge $cooldown";
+
         my $deposit = sprintf("%.2f", rand($maxDeposit));
         say "$d,$playerID,$playerSignup[$ind],$deposit";
         ++$n;
