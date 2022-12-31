e646ca3da70afe8089e12c0c3288ab1bcb4140d0
hellmarbecker <becker@hellmar-becker.de>
2022-12-31 23:16:56 +0100

    Initial

--- generate_sessions_metrics.py
+++ generate_sessions_metrics.py
@@ -1,11 +1,25 @@
+import json
+import time
 from faker import Faker
 
 fake = Faker()
 
-def main:
+def main():
 
-    pass
+    ts = time.time()
+    for sid in range(10000):
 
+        uid = fake.random_int(min=1, max=1000)
+        pagesPerSession = fake.random_int(min=1, max=30)
+        sessionLength = pagesPerSession * (1.0 + fake.random_int(min=1, max=100) / 50.0)
+        
+        emitRecord = {
+            "timestamp" : ts,
+            "uid" : uid,
+            "sessionLength" : sessionLength,
+            "pagesPerSession" : pagesPerSession
+        } 
+        print(json.dumps(emitRecord))
 
 if __name__ == "__main__":
     main()
