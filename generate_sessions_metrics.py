0b8924ac982158b9d56bf7fadc1301bc8eef4b86
hellmarbecker <becker@hellmar-becker.de>
2023-01-02 14:28:20 +0100

    Add multiple random distribution profiles

--- generate_sessions_metrics.py
+++ generate_sessions_metrics.py
@@ -1,21 +1,62 @@
 import json
 import time
+import random
 from faker import Faker
 
+maxUsers = 1000
+maxSessions = 20000
 fake = Faker()
+
+
+def gauss_cut(m, s):
+
+    result = random.gauss(m, s)
+    if (result > 0.0):
+        return result
+    return 0.0
+
+
+def bimodal(m1, m2, s, p):
+
+    if random.random() <= p:
+        m = m1
+    else:
+        m = m2
+    return gauss_cut(m, s)
+
 
 def main():
 
-    ts = time.time() - 10 * 86400 # 10 days back
-    for sid in range(10000):
+    distr = {
+        'sessionLength' : {
+            'A': { 'fn': gauss_cut,          'param': (1000, 200) },
+            'B': { 'fn': gauss_cut,          'param': (1000, 1000) },
+            'C': { 'fn': random.expovariate, 'param': (0.001,) },
+            'D': { 'fn': bimodal,            'param': (1000, 5000, 200, 0.8) },
+        },
+        'pagesPerSession' : {
+            'A': { 'fn': gauss_cut,          'param': (10,  2) },
+            'B': { 'fn': gauss_cut,          'param': (10, 10) },
+            'C': { 'fn': random.expovariate, 'param': (0.1,) },
+            'D': { 'fn': bimodal,            'param': (10, 30, 2, 0.8) },
+        }
+    }
 
-        uid = fake.random_int(min=1, max=1000)
-        pagesPerSession = fake.random_int(min=1, max=30)
-        sessionLength = pagesPerSession * (1.0 + fake.random_int(min=1, max=100) / 50.0)
+
+    segment = [ random.choices('ABCD', cum_weights=(0.50, 0.75, 0.90, 1.00), k=1)[0] for uid in range(maxUsers) ]
+
+    ts = time.time() - 10 * 86400 # 10 days back
+    for sid in range(maxSessions):
+
+        uid = fake.random_int(min=0, max=maxUsers-1)
+        seg = segment[uid]
+        pagesPerSession = 1 + round(distr['pagesPerSession'][seg]['fn'](*distr['pagesPerSession'][seg]['param']))
+        sessionLength = round(distr['sessionLength'][seg]['fn'](*distr['sessionLength'][seg]['param']), 2)
         
         emitRecord = {
-            "timestamp" : ts,
+            "timestamp" : int(round(ts)),
             "uid" : uid,
+            "segment" : seg,
             "sessionLength" : sessionLength,
             "pagesPerSession" : pagesPerSession
         } 
